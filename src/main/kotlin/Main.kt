983bbdb51bc10577a9eaaad25e193e1f1f9b2956
Bryson Zimmerman <brzimmer@nmu.edu>
2024-11-17 21:49:58 -0500

    Add number formatting

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,8 @@
 package technology.zim
 
 import technology.zim.data.Tile
+import java.text.NumberFormat
+import java.util.Locale
 import kotlin.time.measureTime
 
 class HierarchicalPathfinding {
@@ -29,13 +31,13 @@ class HierarchicalPathfinding {
                 MapBackedPathfinder.generatePath(Tile(0, 0), Tile(n - 1, (n - 1)))
             }
 
-
+            val numberFormat = NumberFormat.getInstance(Locale.US)
             println(World.toString())
-            println(n*n)
-            println("Maze build time: ${buildMazeTime.inWholeMilliseconds} ms")
-            println("BFS Pathfinder time: ${bfsPathfinderTime.inWholeMilliseconds}ms")
-            println("Array-Backed Pathfinder time: ${arrayBackedPathfinderTime.inWholeMilliseconds}ms")
-            println("HashMap-Backed Pathfinder time: ${mapBackedPathfinderTime.inWholeMilliseconds}ms")
+            println(numberFormat.format(n*n))
+            println("Maze build time: ${numberFormat.format(buildMazeTime.inWholeMilliseconds)} ms")
+            println("BFS Pathfinder time: ${numberFormat.format(bfsPathfinderTime.inWholeMilliseconds)}ms")
+            println("Array-Backed Pathfinder time: ${numberFormat.format(arrayBackedPathfinderTime.inWholeMilliseconds)}ms")
+            println("HashMap-Backed Pathfinder time: ${numberFormat.format(mapBackedPathfinderTime.inWholeMilliseconds)}ms")
         }
 
         fun buildMaze(n: Int) {
